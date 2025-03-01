df6803a6dbb4ac8703db0d7c037880a4c9545207
HenrikHao <73416544+HenrikHao@users.noreply.github.com>
2025-03-01 18:50:47 +1100

    Initial Commit

---
+++ WordTranslator/WordTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordTranslatorApp.swift
+//  WordTranslator
+//
+//  Created by Henrik Hao on 1/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct WordTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordTranslatorTests/WordTranslatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordTranslatorTests.swift
+//  WordTranslatorTests
+//
+//  Created by Henrik Hao on 1/3/2025.
+//
+
+import Testing
+@testable import WordTranslator
+
+struct WordTranslatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
